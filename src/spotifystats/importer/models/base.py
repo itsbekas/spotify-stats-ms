9600c227ac379f33999825c1b79c242c936291b1
= <bernardo.jordao@tecnico.ulisboa.pt>
2023-11-11 12:14:18 +0000

    initial commit - models

---
+++ src/spotifystats/importer/models/base.py
@@ -0,0 +1,6 @@
+# import base class for models
+
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
---
+++ src/spotifystats/importer/models/play.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Integer, String, DateTime
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import Mapped
+
+from .base import Base
+
+class Play(Base):
+    __tablename__ = 'plays'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    track: Mapped[int] = mapped_column(String)
+    played_at: Mapped[str] = mapped_column(DateTime)
